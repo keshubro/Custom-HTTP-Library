ba80179b0557349eeee14fe226c175c638d518c2
Keshav Sharma <keshavs642@gmail.com>
2020-11-25 20:53:21 +0530

    POST PUT DELETE Requests

--- app.js
+++ app.js
@@ -1,10 +1,60 @@
 const http = new easyHttp();
 
-http.get('https://jsonplaceholder.typicode.com/posts', function(error, posts){
+
+// GET Request
+// http.get('https://jsonplaceholder.typicode.com/posts', function(error, posts){
+//   //This is a callback with two params : error, response(if no error)  
+//     if(error === null)
+//     {
+//         console.log(posts);
+//     }
+//     else
+//     {
+//         console.log(error);
+//     }
+    
+// });
+
+
+
+//Create Data
+const data = {
+    title: 'Custom Post',
+    body: 'This is a custom post'
+};
+
+//POST request
+// http.post('https://jsonplaceholder.typicode.com/posts', data, function(error, post){
+//     if(error === null)
+//     {
+//         console.log(post);
+//     }
+//     else
+//     {
+//         console.log(error);
+//     }
+// });
+
+
+// PUT Request
+http.put('https://jsonplaceholder.typicode.com/posts/5', data, function(error, post){
+    if(error === null)
+    {
+        console.log(post);
+    }
+    else
+    {
+        console.log(error);
+    }
+});
+
+
+//DELETE Request
+http.delete('https://jsonplaceholder.typicode.com/posts/1', function(error, response){
   //This is a callback with two params : error, response(if no error)  
     if(error === null)
     {
-        console.log(posts);
+        console.log(response);
     }
     else
     {
--- easyhttp.js
+++ easyhttp.js
@@ -1,3 +1,10 @@
+/*
+Why do we need a callback for GET request?
+- Suppose we want to return the responseText to the app.js from where the get method was called.
+    If we do it normally, it will give undefined !
+    But if we use a callback, it works !!!
+*/
+
 function easyHttp()
 {
     this.http = new XMLHttpRequest();
@@ -16,7 +23,7 @@ easyHttp.prototype.get = function(url, callback){
         }
         else
         {
-            //If therer is an error, send the error code. And since we do not have any response in this case, we do not need to pass anything.
+            //If there is an error, send the error code. And since we do not have any response in this case, we do not need to pass anything.
             callback('Error: ' + self.http.status);
         }
     }
@@ -24,8 +31,59 @@ easyHttp.prototype.get = function(url, callback){
     this.http.send();
 }
 
+
 // Make an HTTP POST request
+easyHttp.prototype.post = function(url, data, callback){
+    this.http.open('POST', url, true);
 
-// Make an HTTP PUT request
+    this.http.setRequestHeader('Content-type', 'application/json');
 
-// Make an HTTP DELETE request
+    let self = this; //To pertain the scope of the object
+    this.http.onload = function(){
+        callback(null, self.http.responseText);
+    }
+
+    //For POST method, we need to send the data in JSON format
+    this.http.send(JSON.stringify(data));
+}
+
+
+
+// Make an HTTP PUT request - Similar to POST
+easyHttp.prototype.put = function(url, data, callback){
+    this.http.open('PUT', url, true);
+
+    this.http.setRequestHeader('Content-type', 'application/json');
+
+    let self = this; //To pertain the scope of the object
+    this.http.onload = function(){
+        callback(null, self.http.responseText);
+    }
+
+    //For POST method, we need to send the data in JSON format
+    this.http.send(JSON.stringify(data));
+}
+
+
+
+// Make an HTTP DELETE request - Similar to GET
+easyHttp.prototype.delete = function(url, callback){
+    this.http.open('DELETE', url, true);
+
+    let self = this; //To pertain the scope of the object
+    this.http.onload = function(){
+        if(self.http.status === 200)
+        {
+            // If there is no error, send the error param values as null
+            // callback(null, self.http.responseText); //self.http.responseText will be empty
+            callback(null, 'Post Deleted');
+        }
+        else
+        {
+            //If there is an error, send the error code. And since we do not have any response in this case, we do not need to pass anything.
+            callback('Error: ' + self.http.status);
+        }
+    }
+
+    this.http.send();
+}
